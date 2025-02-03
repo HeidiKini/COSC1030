37a7a18e0befc668fb93dd8e8b3682625ed6c5bf
HeidiKini <heididuckwitz@westernwyoming.edu>
2025-02-03 02:29:10 +0000

    Finished assignment restaurant-bill.

--- restaurant-bill/main.cpp
+++ restaurant-bill/main.cpp
@@ -1,20 +1,25 @@
 //
-// Name
-// Date
+// Heidi Kinikini
+// 02/02/2025
 // Restaurant Bill Programming Project
 // COSC 1030
 //
+#include <iostream>
+using namespace std;
+int main ()
+ {
+    //Variable declarations and calculations
+    double meal = 88.67;
+    double tax = meal * 0.0675;  //tax calculation
+    double subtotal = meal + tax; //tax plus total calculation
+    double tip = subtotal * 0.20; //subtotal plus tip calculation
+    double total = subtotal + tip; //total calculation
 
-// Variable declarations
-
-// Constants for the tax rate and tip percentage
-
-// Get the amount of the purchase.
-
-// Calculate the sales tax.
-
-// Calculate the tip.
-
-// Calculate the total of the sale.
-
-// Print information about the sale.
+    //Print information about the sale
+    cout << "Meal cost: " << meal << endl;
+    cout << "Tax: " << tax << endl;
+    cout << "Tip: " << tip << endl;
+    cout << "Total: "<< total << endl;
+        
+    return 0;
+ }
