e5c521ce075c2e0aa9615dae38fac8862a685a95
HeidiKini <heididuckwitz@westernwyoming.edu>
2025-04-08 22:15:23 +0000

    first commit for assignment pointer rewrite not fi nished

--- pointer-rewrite/main.cpp
+++ pointer-rewrite/main.cpp
@@ -1,7 +1,41 @@
 //
-// Name
-// Date
+// Heidi Kinikini
+// 4/8/2025
 // Pointer Rewrite Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+//function prototype
+int doSomething(int &, int &);
+
+int main()
+{
+    int x = 10, y = 2;
+
+    cout << "Before calling the function:\n"
+    << "x = " << endl
+    << "y = " << endl << endl;
+
+    cout << "The function returns "
+    << doSomething(x, y) << endl << endl;
+
+    cout << "After calling the function:\n"
+    << "x = " << x << endl 
+    << "y = " << y << endl << endl;
+
+    return 0;
+
+}
+
+//function doSomething
+
+int doSomething(int &x, int &y)
+{
+    int temp = x;
+    x = y * 10;
+    y = temp * 10;
+    return x + y;
+}
