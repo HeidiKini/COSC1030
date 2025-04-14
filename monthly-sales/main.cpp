ed8114c736ef497c112cc1d8d817dbdb32ab6899
HeidiKini <heididuckwitz@westernwyoming.edu>
2025-04-14 07:41:21 +0000

    added comments I also added an if statement instea d of a ternary.

--- monthly-sales/main.cpp
+++ monthly-sales/main.cpp
@@ -8,6 +8,7 @@
 #include <iostream> 
 using namespace std;
 
+//Function that calculates the total
 double calculateTotal(double* sales, int size)
 {
     double sum = 0;
@@ -19,32 +20,44 @@ double calculateTotal(double* sales, int size)
     return sum;
 }
 
+//Function that calculates the average
 double averageCalculator(double* sales, int size)
 {
     double sum = calculateTotal(sales, size);
     
-    return size > 0 ? sum / size : 0;
+    if (size > 0)
+    {
+        return sum / size;
+
+    } else 
+
+    {
+        return 0;
+    }
 
 }
 
 int main ()
 {
     int numberOfMonths;
-
+    //get the number of months from the user
     cout << "Please enter the number of months: ";
     cin >> numberOfMonths;
 
+    //Allocate new memory to sales equal to number of months
     double* sales = new double[numberOfMonths];
 
+    //Go through number of months with a for loop to get each sell for however many number of months.
     for (int i = 0; i < numberOfMonths; i++)
     {
         cout << "Enter the sales for the month " << i + 1 << ": ";
         cin >> sales[i];
     }
-
+    // call the functions with the user input as parameters to calculate total and average
     double yearlyTotal = calculateTotal(sales, numberOfMonths);
     double average = averageCalculator(sales, numberOfMonths);
 
+    //Display the output
     cout << "Total yearly sales: " << yearlyTotal << endl;
     cout << "Average monthly sales: " << average << endl;
 
